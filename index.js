96846451c05b6a3da101b6ad6f11a753f06a1208
Rob <robertanderson112233@gmail.com>
2023-06-14 10:58:34 +0100

    arranged and tested movesData

--- index.js
+++ index.js
@@ -3,17 +3,12 @@ const { fetchSinglePokemon } = require("./__utils__/api");
 const getAllPokemon = () => {
   const pokemonArr = [];
   for (let i = 1; i <= 10; i++) {
-    // fetchSinglePokemon(i).then((pokemon) => {
-    //   pokemonArr.push(pokemon);
-    // });
     pokemonArr.push(fetchSinglePokemon(i));
   }
   let x;
   return Promise.all(pokemonArr).then((result) => {
-    // x = result;
     return result;
   });
-  return x;
 };
 
 module.exports = {
